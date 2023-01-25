575f87bd8e2663643a3c84756edd2c449e4e2df4
Trey <treyfields351@gmail.com>
2023-01-25 16:48:16 -0500

    commit tree 5333b419f0f858a86e05481d4f1edb6d156f5bf2

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert()
+{
+    alert('I was clicked!');
+}
+
+function addingEventListener()
+{
+    input.addEventListener('click', clickAlert);
 }
